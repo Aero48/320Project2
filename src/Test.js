ec6c2fc0a69cf4106b9d35431f28ab20454913cd
Aero48 <56200404+Aero48@users.noreply.github.com>
2021-11-12 20:27:32 -0500

    Update Test.js

--- src/Test.js
+++ src/Test.js
@@ -1,4 +1,21 @@
+import react, { useState, useEffect } from "react";
+
 export default function Test(){
+
+    const [locations, setLocations] = useState([]);
+
+    useEffect(() => {
+        fetch("../public/data/data.json")
+          .then((result) => result.json())
+          .then((data) => {
+            //store data
+            setLocations(data);
+          });
+      }, []);
+
+      console.log(locations);
+      //const firstName = locations[0].name
+
     return (
         <div>
             <h1>Test</h1>
