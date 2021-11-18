c02da9a4d5f7c36b7542ecea309b3520bbead8b8
Aero48 <56200404+Aero48@users.noreply.github.com>
2021-11-18 18:47:32 -0500

    Code Cleanup
    -Changed the name of the main component
    -Added more comments to the code

--- src/MainList.js
+++ src/MainList.js
@@ -1,11 +1,12 @@
 import react, { useState, useEffect } from "react";
 import {List, ListItem, Button, Stack, Divider, Container, Grid} from "@mui/material";
 
-export default function Test(){
+export default function MainList(){
 
-    const [locations, setLocations] = useState([]);
-    const [currentItem, setCurrentItem] = useState({});
+    const [locations, setLocations] = useState([]); //List of locations from json file
+    const [currentItem, setCurrentItem] = useState({}); //Contains data from the selected location
 
+    //Pulls the data from the json file
     useEffect(() => {
         fetch("data/data.json")
           .then((result) => result.json())
@@ -15,9 +16,7 @@ export default function Test(){
           });
       }, []);
 
-      console.log(locations);
-      //const firstName = locations[0].name
-
+      //Maps out divs containing data from locationsList
       const locationList = locations.map((place, ind) => (
          <div key={ind}>
            <img src={place.image} width="200px"/>{place.name}
@@ -48,7 +47,8 @@ export default function Test(){
             </Grid>
         </div>
     );
-
+    
+    //Sets the current item to the one that was clicked on
     function showDescription(itemId){
       setCurrentItem(locations[itemId])
     }
